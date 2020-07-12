74da6b4c94de60b73dc5a0ee9052d6174bdcd148
Ivan Vilanculo <vilanculoivan@gmail.com>
2020-07-12 18:35:25 +0200

    init express back-end project

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import consola from 'consola'
+import express from 'express'
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(port, () => {
+  consola.info(`Server started at http://localhost:${port}`)
+})
