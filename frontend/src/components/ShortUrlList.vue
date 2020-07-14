61eeb0df05c1d9da875150a61193eac4ee32a6bf
Ivan Vilanculo <vilanculoivan@gmail.com>
2020-07-14 18:32:22 +0200

    Improve recent URLs style

--- frontend/src/components/ShortUrlList.vue
+++ frontend/src/components/ShortUrlList.vue
@@ -4,19 +4,29 @@
       <tr>
         <th>Url</th>
         <th>Short Url</th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
       <tr v-for="url in urls" :key="url._id">
         <td>
           <div>
-            <a :href="url.url" :title="url.url" target="_blank">{{ url.url }}</a>
+            <a :href="url.url" :title="url.url" target="_blank">
+              {{ url.url }}
+            </a>
           </div>
         </td>
         <td>
           <div>
-            <a :href="url.shortUrl" :title="url.shortUrl" target="_blank">{{ url.shortCode }}</a>
+            <a :href="url.shortUrl" :title="url.shortUrl" target="_blank">
+              {{ url.shortCode }}
+            </a>
           </div>
+        </td>
+        <td>
+          <button class="copy-button" title="Copy">
+            <copy-icon />
+          </button>
         </td>
       </tr>
     </tbody>
@@ -25,13 +35,15 @@
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
 import { Url } from '@/store/createShortUrlsStore'
+import CopyIcon from '@/components/svg/Copy.vue'
 export default defineComponent({
+  components: { CopyIcon },
   props: {
     urls: { type: Array as () => Url[] }
   }
 })
 </script>
-<style>
+<style scoped>
 table {
   overflow: hidden;
   table-layout: fixed;
@@ -44,13 +56,15 @@ thead {
 tbody {
   background-color: var(--color-white);
 }
-th, td {
-  padding: 0.5rem .75rem;
-  letter-spacing: .05em;
-  white-space: nowrap;
-  text-overflow: ellipsis;
-  overflow: hidden;
+th,
+td {
+  padding: 0.5rem 0.75rem;
+  letter-spacing: 0.05em;
   border-bottom: 1px solid var(--color-gray-300);
+}
+th:last-child,
+td:last-child {
+  width: 40px;
 }
 th {
   text-align: left;
@@ -60,5 +74,21 @@ th {
 }
 tr:hover td {
   background-color: var(--color-gray-100);
+}
+a {
+  display: block;
+  width: 100%;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
+.copy-button {
+  appearance: none;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  color: var(--color-gray-600);
+  margin-right: 2px;
+  margin-left: 2px;
 }
 </style>
--- frontend/src/components/UrlInput.vue
+++ frontend/src/components/UrlInput.vue
@@ -56,7 +56,8 @@ export default defineComponent({
   border-radius: 3px;
 }
 .url-input--focused {
-  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)
+  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
+    0 4px 6px -2px rgba(0, 0, 0, 0.05);
 }
 .url-input input {
   flex: 1 1;
